211b20723ece5237ca47f328e082dcb7d7ce20ed
Kyle Jennings <kjennings.dev@gmail.com>
2018-11-09 13:40:05 -0600

    Adds initial e2e testing for helix

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,11 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to minerva!');
+    expect(page.getParagraphText()).toEqual('Rackspace Intelligence');
+  });
+
+  it('should display Helix UI', () => {
+    page.navigateTo();
+    expect(page.getHelixNavCssClass()).toBe('hxNav');
   });
 });
