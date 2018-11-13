a3c7927dccb01ba753de715f54c3ebcb493c2b89
Kyle Jennings <kjennings.dev@gmail.com>
2018-11-13 15:26:14 -0600

    Adds compudoc and configures the gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,11 @@ var gulp = require('gulp');
 
 // Require task files
 var sample = require('./tasks/sample');
-var jasmine = require('./tasks/jasmine');
+var compudoc = require('./tasks/compudoc');
 
 // Connect task files to gulp
 gulp.task('sample', sample.run());
-gulp.task('jasmine', jasmine.run());
-
+gulp.task('compudoc', compudoc.run());
 
 gulp.task('default', ['sample']);
-gulp.task('test:unit', ['jasmine']);
+gulp.task('compudoc', ['compudoc']);
