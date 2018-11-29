98dfea80bf1826059753184e43281472da190612
Kyle Jennings <kjennings.dev@gmail.com>
2018-11-29 09:48:37 -0600

    Adds a logging service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { environment } from 'src/environments/environment';
 
 import { PocDataCallComponent } from './poc-data-call/poc-data-call.component';
 import { AppModuleMock } from './app.module.mock';
+import { LoggingService } from './logging.service';
 
 @NgModule({
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
@@ -28,6 +29,12 @@ import { AppModuleMock } from './app.module.mock';
       useFactory: portalData,
       multi: true,
       deps: []
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: logger,
+      multi: true,
+      deps: []
     }
   ],
   bootstrap: [ AppComponent ]
@@ -38,6 +45,16 @@ export class AppModule {}
 export function portalData(): any {
   if (!environment.production) {
     Window['PORTAL_DATA'] = new AppModuleMock().mock
+  }
+  return () => {};
+}
+
+export function logger(): any {
+  var logger = new LoggingService();
+  // if we don't currently have a log level set
+  // set the level to a default of error
+  if (!logger.getLevel()){
+    logger.setLevel(logger.logLevels.error);
   }
   return () => {};
 }
