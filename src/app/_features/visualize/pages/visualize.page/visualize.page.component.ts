d11a2a390ebffcdc38f6086d1d8549ad95bf6f2f
FarajiA <sacbeme04@yahoo.com>
2019-07-29 15:54:06 -0500

    Add hedwig via github tar + logic for adding graph

--- src/app/_features/visualize/pages/visualize.page/visualize.page.component.ts
+++ src/app/_features/visualize/pages/visualize.page/visualize.page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GraphEngine } from 'hedwig-monitoring-library';
 
 @Component({
   selector: 'app-visualize.page',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisualizePage implements OnInit {
 
+  graphType: string;
+  dataStuff = JSON.stringify([
+    {
+        "usage_average": .37,
+        "time": "2018-11-24T18:58:21Z"
+    },
+    {
+        "usage_average": 0.50,
+        "time": "2018-11-25T23:58:21Z"
+    },
+    {
+        "usage_average": .80,
+        "time": "2018-11-26T02:58:21Z"
+    },
+    {
+        "usage_average": 0.12,
+        "time": "2018-11-27T10:58:21Z"
+    }]);
   constructor() { }
 
   ngOnInit() {
+    //needed on components that utilize Hedwig
+    new GraphEngine();
+    this.graphType = 'cpu-average-usage';
   }
 
 }
