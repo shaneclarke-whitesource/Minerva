71d8870cde9060581a924483cefefaa6f9ca2f8f
FarajiA <sacbeme04@yahoo.com>
2019-11-26 15:12:44 -0600

    update Helix -> .018.0-rc.2

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import * as HelixUI from "helix-ui";
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,5 +9,6 @@ if (environment.production) {
   enableProdMode();
 }
 
+HelixUI.default.initialize();
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
