2ffa1120ea7294a90b6b2bdbf1858dccc195603f
FarajiA <sacbeme04@yahoo.com>
2020-03-19 08:11:09 -0500

    demo adding yargs

--- localenv/arguments.js
+++ localenv/arguments.js
@@ -1,3 +1,4 @@
+/*
 const commander = require('commander');
 
 commander
@@ -7,3 +8,13 @@ commander
     .parse(process.argv);
 
 module.exports = commander;
+*/
+
+const args = require('yargs').argv
+
+const body = {
+    username: args.username,
+    password: args.password
+};
+
+module.exports = body;
