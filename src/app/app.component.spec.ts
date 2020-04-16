312e817ddfd1ca1770a9b01ecfb2a5f4113038be
prashant mishra <prashant.mishra@rackspace.com>
2020-04-16 16:57:55 +0530

    remove unused code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,7 +24,6 @@ describe('AppComponent', () => {
   });
 
   it('should create the app', () => {
-    // const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { LoggingService } from './_services/logging/logging.service';
 
 @Component({
