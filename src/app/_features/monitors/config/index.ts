9f256e0b3e1445105e905ee0d3d2c3b721c83e14
FarajiA <sacbeme04@yahoo.com>
2020-04-26 23:43:35 -0500

    Disk monitor config settings

--- src/app/_features/monitors/config/index.ts
+++ src/app/_features/monitors/config/index.ts
@@ -1,5 +1,7 @@
 import { CPUConfig } from './cpu';
+import { DiskConfig } from './disk';
 
 export const config = {
-    Cpu: new CPUConfig()
+    Cpu: new CPUConfig(),
+    Disk: new DiskConfig()
 }
