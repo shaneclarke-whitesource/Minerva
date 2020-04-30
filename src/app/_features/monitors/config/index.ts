bedab0bc4d7991470cda4e28cfb58bc38bb1872c
prashant mishra <prashant.mishra@rackspace.com>
2020-05-01 00:55:50 +0530

    network config file added

--- src/app/_features/monitors/config/index.ts
+++ src/app/_features/monitors/config/index.ts
@@ -1,7 +1,9 @@
 import { CPUConfig } from './cpu';
 import { DiskConfig } from './disk';
+import { NetworkConfig } from './net';
 
 export const config = {
     Cpu: new CPUConfig(),
-    Disk: new DiskConfig()
+    Disk: new DiskConfig(),
+    Net: new NetworkConfig()
 }
