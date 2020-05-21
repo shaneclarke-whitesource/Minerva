3e74ec90d958fe762a70bfcc55591cb8576d5b3d
sowmya24 <sowmya.v@rackspace.com>
2020-05-21 15:00:40 +0530

    List Of Monitors

--- e2e/src/specs/listOfResources.e2e-spec.ts
+++ e2e/src/specs/listOfResources.e2e-spec.ts
@@ -3,8 +3,7 @@ import { browser, element, by } from "protractor";
 import { AppPage } from "../pageObjects/app.po";
 import { async } from "@angular/core/testing";
 import { LoadResources } from "../feature/Resources/ListOfResources";
-import * as fs from 'fs';
-import * as path from 'path';
+
 
 describe("Resources List", () => {
     let page: AppPage;
@@ -16,19 +15,19 @@ describe("Resources List", () => {
     page.navigateTo();
   });
 
-  it("Check if check box is enabled" , async() => {
+  it("Check if check box is enabled" , () => {
     
     expect(element(by.xpath("//th[@class='hxControl']//hx-checkbox-control//label//hx-checkbox")).isPresent());
   });
 
-  it("Check if Delete button is enabled" , async() => {
+  it("Check if Delete button is enabled" , () => {
     
     element.all(by.xpath("//th[@class='hxControl']//hx-checkbox-control//label//hx-checkbox")).click();
     browser.sleep(20000);
     expect(element(by.xpath("//button[contains(text(),'Delete')]")).isEnabled()).toBe(true);
   });
 
-  it("check if Create Muiltiple Monitores button is enabled", async() => {
+  it("check if Create Muiltiple Monitores button is enabled", () => {
     
     
         element.all(by.xpath("//th[@class='hxControl']//hx-checkbox-control//label//hx-checkbox")).click();
@@ -37,37 +36,37 @@ describe("Resources List", () => {
        
   });
 
-  it("check if Create Supression button is enabled",  async() => {
+  it("check if Create Supression button is enabled",  () => {
     
    element.all(by.xpath("//th[@class='hxControl']//hx-checkbox-control//label//hx-checkbox")).click();
     browser.sleep(20000);
     expect(element(by.xpath("//button[contains(text(),'Create Suppression')]")).isEnabled()).toBe(true);
   });
- it("should have Add resources button", async() => {
+ it("should have Add resources button", () => {
    
   expect(element(by.xpath("//hx-disclosure[@id='addResButton']")).isEnabled()).toBe(true);
 
  });
- it("Once add resources should have Submit button enabled", async()=>{
+ it("Once add resources should have Submit button enabled", ()=>{
    element(by.xpath("//hx-disclosure[@id='addResButton']")).click();
   expect(element(by.xpath("//button[@class='hxBtn hxPrimary']")).isEnabled()).toBe(true);
 
  });
- it("Once Add resources button is clicked cancel button should be enabled", async()=>{
+ it("Once Add resources button is clicked cancel button should be enabled", ()=>{
   element(by.xpath("//hx-disclosure[@id='addResButton']")).click();
   expect(element(by.xpath("//button[contains(text(),'Cancel')]")).isEnabled()).toBe(true);
   
  });
- it("Add Resources button clicked should have text box",async()=>{
+ it("Add Resources button clicked should have text box",()=>{
   element(by.xpath("//hx-disclosure[@id='addResButton']")).click();
   expect(element(by.xpath("//input[@id='txtResource']")).isPresent()).toBe(true);
  });
- it("Should have enabled Presence Monitor checkbox", async()=>{
+ it("Should have enabled Presence Monitor checkbox", ()=>{
   element(by.xpath("//hx-disclosure[@id='addResButton']")).click();
   
   expect(element(by.xpath("//label[contains(text(),'Enable Presence Monitoring')]")).isPresent()).toBe(true);
  });
- it("Should have search option", async()=>{
+ it("Should have search option", ()=>{
    expect(element(by.xpath("//input[@id='txtSearch']")).isPresent()).toBe(true);
 });
 
