5ece504dc853d8cdb4f5a718ca7c3787f9e18b71
Lovedeep Singh <love7875@Lovedeeps-MacBook-Pro.local>
2020-06-16 04:53:50 -0700

    added test case for changeLoadingStatus() function in spinner service MNRVA-499

--- src/app/_services/spinner/spinner.service.spec.ts
+++ src/app/_services/spinner/spinner.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SpinnerService } from './spinner.service';
+import { doesNotReject } from 'assert';
 
 describe('SpinnerService', () => {
   let service: SpinnerService;
@@ -18,6 +19,24 @@ describe('SpinnerService', () => {
      service.isLoading.subscribe((value) => {
       expect(value).toBe(false);
      }); 
+  });
+
+  it('changeLoadingStatus() should emit boolean value to loadingSource subject as false', (done) => {
+    const flag = false;
+    service.changeLoadingStatus(flag);
+    service.isLoading.subscribe((value) => {
+        expect(value).toBe(flag);
+        done();
+      })
+  });
+
+  it('changeLoadingStatus() should emit boolean value to loadingSource subject as true', (done) => {
+    const flag = true;
+    service.changeLoadingStatus(flag);
+    service.isLoading.subscribe((value) => {
+      expect(value).toBe(flag);
+      done();
+    });
   });
 
 
