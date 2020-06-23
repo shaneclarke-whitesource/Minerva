67468dae6ef0e085714e2aebdab34147991046fc
Lovedeep Singh <love7875@Lovedeeps-MacBook-Pro.local>
2020-06-23 21:10:03 +0530

    added loader to bound resource list section MNRVA-514

--- src/app/_features/monitors/components/resource_list/resource-list.component.ts
+++ src/app/_features/monitors/components/resource_list/resource-list.component.ts
@@ -2,13 +2,19 @@ import { Component, OnInit, Input } from '@angular/core';
 import { BoundMonitor } from 'src/app/_models/resources';
 import { MonitorService } from '../../../../_services/monitors/monitor.service';
 import { Subscription } from 'rxjs';
-import { ConstantPool } from '@angular/compiler';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-resource-list',
   templateUrl: './resource-list.component.html',
   styleUrls: ['./resource-list.component.scss']
 })
 export class ResourceListComponent implements OnInit {
+  total: number;
+  page: number = 0;
+  perPageElements: number = 5;
+
+  isLoading: boolean = false;
+  //perPage: number = environment.pagination.pageSize;
   resources:BoundMonitor[]; 
   @Input() monitorId:string;
   subscriber = new Subscription();
@@ -28,10 +34,41 @@ export class ResourceListComponent implements OnInit {
   */
 
   getResources(){
-    this.subscriber=this.mntor.getBoundMonitor({monitorId : this.monitorId}).subscribe(data =>{      
+    this.subscriber=this.mntor.getBoundMonitor({monitorId : this.monitorId, size: this.perPageElements, page: this.page}).subscribe(data =>{     
       this.resources = data.content;
+      this.total = data.totalElements;
+      this.isLoading = false;
     })
   }
+
+    /**
+   * @description <app-pagination>
+   * @param n number
+   * @returns void
+  */
+ goToPage(n: number): void {
+  this.page = n;
+  this.isLoading = true;
+  this.getResources();
+}
+
+/**
+ * @description <app-pagination>
+ */
+nextPage(): void {
+  this.page++;
+  this.isLoading = true;
+  this.getResources();
+}
+
+/**
+ * @description <app-pagination>
+ */
+prevPage(): void {
+  this.page--;
+  this.isLoading = true;
+  this.getResources();
+}
 
   /**
    * @description call ngOnDestroy function when to unsubscribe subscriber param which is subscribed to fetch resources. 
