0d7faf0848f71e3d5fc238de176d02efdd25278c
Lovedeep Singh <love7875@Lovedeeps-MBP.bbrouter>
2020-07-30 19:14:33 +0530

    removed authguard from tabset module

--- projects/admin/src/app/tab/tabset-routing.module.ts
+++ projects/admin/src/app/tab/tabset-routing.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {
-  AuthGuardService as AuthGuard
-} from '../auth/auth-gaurd.service';
 import { TabsetComponent } from './tabset.component';
 import { AdminResourceDetailsPage } from './../../app/_features/resources/pages/details/admin-resource-details.page';
 
@@ -10,7 +7,6 @@ const routes: Routes = [
   {
     path: '', 
     component: TabsetComponent,
-    canActivate:[AuthGuard]
  },
  {
   path: 'resources/:id', 
